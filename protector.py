efc9a1eeba9d0437e2dd26fec7e0ca6c8e5fcdb0
Michał Sidor <michcioperz@gmail.com>
2015-08-27 00:55:53 +0200

    uh

--- protector.py
+++ protector.py
@@ -14,7 +14,8 @@ class Show(object):
         return [Episode(self, i) for i in os.listdir(os.path.join(self.storage.path, self.codename)) if os.path.isfile(os.path.join(self.storage.path, self.codename, i)) and str(mimetypes.guess_type(os.path.join(self.storage.path, self.codename, i), strict=False)[0]).startswith("video/")]
 
 class Storage(object):
-    def __init__(self, path):
+    def __init__(self, path, codename):
         self.path = path
+        self.codename = codename
     def shows(self):
         return [Show(self, h) for h in os.listdir(self.path) if os.path.isdir(os.path.join(self.path, h)) and len([e for e in [str(mimetypes.guess_type(os.path.join(self.path, h, f), strict=False)[0]) for f in os.listdir(os.path.join(self.path, h))] if e.startswith("video/")]) > 0] # TODO drop the bass
