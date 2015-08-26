4bba4a8ba903a36c0dace0fa0ab5a454b894546d
Michał Sidor <michcioperz@gmail.com>
2015-08-26 23:48:59 +0200

    slow progress on internal design

---
+++ protector.py
@@ -0,0 +1,19 @@
+import mimetypes, os
+
+
+class Episode(object):
+    def __init__(self, show, filename):
+        self.show = show
+        self.filename = filename
+
+class Show(object):
+    def __init__(self, storage, codename):
+        self.storage = storage
+    def episodes(self):
+        return [Episode(self, i) for i in os.listdir(os.path.join(self.storage.path, self.codename)) if os.path.isfile(i) and str(mimetypes.guess_type(os.path.join(self.storage.path, self.codename, i), strict=False)[0]).startswith("video/")]
+
+class Storage(object):
+    def __init__(self, path):
+        self.path = path
+    def shows(self):
+        return [Show(self, h) for h in os.listdir(self.path) if os.path.isdir(g) and len([e for e in [str(mimetypes.guess_type(f, strict=False)[0]) for f in os.listdir(os.path.join(self.path, h))] if e.startswith("video/")]) > 0] # TODO drop the bass
